603eb1422a1fb6f9d21d3f829e9335ec82d0b5f4
JeffDinsmore <hello@jeffdinsmore.com>
2020-10-30 13:31:04 -0700

    successfully added a random feature to get method

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -89,15 +89,12 @@ namespace AnimalShelter.Controllers
     [HttpGet("Random")]
     public ActionResult<Animal> Get()
     {
-      List<Animal> model = _db.Animals.Reverse().ToList();
-      int maxValue = model.FirstOrDefault(x => x.Name).AnimalId;
-      // var max = model.OrderByDescending(model => model.AnimalId).First();
-      // int maxValue = max.FirstOrDefault(AnimalId);
-      // var max = model.Where(x => x.AnimalId == model.Max(y => y.AnimalId));
-      // string george = model.AsQueryable().Max();
-      // int maxValue = Int32.Parse(max);
-      int minimumValue = 1;
-      int maximumValue = maxValue;
+      List<Animal> model = _db.Animals.ToList();
+      var sortedList = model.OrderByDescending(x => x.AnimalId);
+      var maxValue = sortedList.First().AnimalId;
+      var minValue = model.First().AnimalId;
+      int minimumValue = minValue;
+      int maximumValue = maxValue + 1;
       Random random = new Random();
       int randomId = random.Next(minimumValue, maximumValue);
       return _db.Animals.FirstOrDefault(entry => entry.AnimalId == randomId);
