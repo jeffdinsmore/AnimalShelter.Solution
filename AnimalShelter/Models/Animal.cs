b940de487557340105f90aefdf3c402891d1a477
JeffDinsmore <hello@jeffdinsmore.com>
2020-10-30 09:05:35 -0700

    built out the context models file

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -2,5 +2,20 @@ using System.ComponentModel.DataAnnotations;
 
 namespace AnimalShelter.Models
 {
-
+public class Animal
+  {
+    public int AnimalId { get; set; }
+    [Required]
+    [StringLength(20)]
+    public string Name { get; set; }
+    [Required]
+    public string Species { get; set; }
+    [Required]
+    public string Breed { get; set; }
+    [Required]
+    [Range(0, 40, ErrorMessage = "Age must be between 0 and 40.")]
+    public int Age { get; set; }
+    [Required]
+    public string Gender { get; set; }
+  }
 }
--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -8,6 +8,22 @@ namespace AnimalShelter.Models
       : base(options)
     {
     }
-
+    public DbSet<Animal> Animals { get; set; }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Animal>()
+        .HasData(
+          new Animal { AnimalId = 1, Name = "Macy", Species = "Dog", Breed = "Cavapoo", Age = 4, Gender = "Female" },
+          new Animal { AnimalId = 2, Name = "Lucy", Species = "Dog", Breed = "Cocker Spaniel", Age = 2, Gender = "Female" },
+          new Animal { AnimalId = 3, Name = "River", Species = "Cat", Breed = "Tabby", Age = 1, Gender = "Female" },
+          new Animal { AnimalId = 4, Name = "Zeus", Species = "Dog", Breed = "German Sheppard", Age = 3, Gender = "Male" },
+          new Animal { AnimalId = 5, Name = "Dave", Species = "Cat", Breed = "Birman", Age = 1, Gender = "Male" },
+          new Animal { AnimalId = 6, Name = "Carl", Species = "Cat", Breed = "American Bobtail", Age = 1, Gender = "Male" },
+          new Animal { AnimalId = 7, Name = "Suzie", Species = "Cat", Breed = "Bengal", Age = 1, Gender = "Female" },
+          new Animal { AnimalId = 8, Name = "Rex", Species = "Dog", Breed = "Australian Kelpie", Age = 2, Gender = "Male" },
+          new Animal { AnimalId = 9, Name = "Winnie", Species = "Cat", Breed = "Bombay", Age = 1, Gender = "Female" },
+          new Animal { AnimalId = 10, Name = "Cocoa", Species = "Cat", Breed = "Burmese", Age = 1, Gender = "Male" }
+        );
+    }
   }
 }
